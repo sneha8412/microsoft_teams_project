234b5a2aeae2d5bbda24e622038c4ff21ac7ddf9
Sneha Agarwal <68921168+sneha8412@users.noreply.github.com>
2022-04-04 17:09:02 -0700

    Popup.js commented out

--- popup.js
+++ popup.js
@@ -1,31 +1,31 @@
-const notifyAll = document.getElementById('notify-button');
-const checkPermission = document.getElementById('check-button');
-const clearAll = document.getElementById('clear-button');
+// const notifyAll = document.getElementById('notify-button');
+// const checkPermission = document.getElementById('check-button');
+// const clearAll = document.getElementById('clear-button');
 
 
-//-------Notify Button-------------
+// //-------Notify Button-------------
 
-notifyAll.click(function(){
-    Push.create('Transferred by', {
-        body: 'Jane Smith is calling',
-        icon: '/icons/Pikachu-128.png',
-        timeout: 2000,
-        onClick: function() {
-            window.focus();
-            this.close();
-        }
-    });
+// notifyAll.click(function(){
+//     Push.create('Transferred by', {
+//         body: 'Jane Smith is calling',
+//         icon: '/icons/Pikachu-128.png',
+//         timeout: 2000,
+//         onClick: function() {
+//             window.focus();
+//             this.close();
+//         }
+//     });
 
-});
+// });
 
-//--------Check-Permission---------
+// //--------Check-Permission---------
 
-checkPermission.click(function(){
-    console.log(Push.Permission.has());
-});
+// checkPermission.click(function(){
+//     console.log(Push.Permission.has());
+// });
 
-//--------Clear-All----------------
+// //--------Clear-All----------------
 
-clearAll.click(function(){
-    Push.clear();
-});
+// clearAll.click(function(){
+//     Push.clear();
+// });
